b951b6d73008f6455f6cbb4b5f7911f90d1be81c
Myron Holmes <1dequanh@gmail.com>
2024-01-17 16:44:30 -0500

    arrays and simplified for loops

---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf", "Tennis", "Cricket"];
+
+sportsTwo.push("Baseball");
+sportsTwo.push("Football");
+
+for (let typeSport of sportsTwo){
+    console.log(typeSport)
+}
---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,10 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let typeSport of sportsOne){
+   if(typeSport == "Golf"){
+    console.log(typeSport + " <<< This is my favorite")
+   }
+   else{
+    console.log(typeSport);
+   }
+}
