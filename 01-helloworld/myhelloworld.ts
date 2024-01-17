21bc24dbc58435994bd187e7904da68428458108
Myron Holmes <1dequanh@gmail.com>
2024-01-17 14:37:55 -0500

    hello world created with steps in comments.

---
+++ 01-helloworld/myhelloworld.ts
@@ -0,0 +1,9 @@
+// Step 1: create myhelloworld.ts file
+// Step 2: console log hello world
+// Step 3: in 01-helloworld folder run 'tsc myhelloworld.ts' (typescript compiler) to generate .js file
+// Step 4: run 'node myhelloworld.js' to print to the console
+console.log("Hello World");
+
+// Step 5: create 2nd console log (only make changes to .ts file)
+// Step 6: repeate step 3 and 4
+console.log("2nd Hello World");
